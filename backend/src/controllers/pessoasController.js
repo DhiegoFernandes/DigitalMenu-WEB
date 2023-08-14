006d64632ecd312802a1974c215a83847fb1d043
Guigite <guicunhaalves@gmail.com>
2023-08-14 12:06:54 -0300

    03

--- backend/src/controllers/pessoasController.js
+++ backend/src/controllers/pessoasController.js
@@ -2,8 +2,7 @@ const pessoasModel = require('../models/pessoasModel');
 
 console.log(pessoasModel.get.getPessoas);
 
-const getAll = (req, res) => {
-    
+const getAll = (req, res) => {  
     const pessoas =  pessoasModel.get.getPessoas();
     return res.status(200).json(pessoas)
 };
