358de86d8f31ca7d7529a80ea08cb1ee682a4fbb
João Victor Fernandes de Andrade <joaovictorandradis@gmail.com>
2023-09-03 01:19:39 -0300

    Finalizando relatorio!

--- backend/src/controllers/itemController.js
+++ backend/src/controllers/itemController.js
@@ -3,10 +3,10 @@ const itemModel = require('../models/itemModel');
 const connection = require('../connection/connection');
 
 exports.insertItem = async(req,res) =>{
-    const{id_pedido, id_produto,qtde,subtotal,observacao} = req.body;
+    const{id_pedido, id_produto,qtde,observacao} = req.body;
 
     try {
-        const item = await itemModel.adicionaItem(id_pedido, id_produto,qtde,subtotal,observacao);
+        const item = await itemModel.adicionaItem(id_pedido, id_produto,qtde,observacao);
         res.status(201).json(item);
     } catch (error) {
         console.error(error);
