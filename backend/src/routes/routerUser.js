8cdd6bca6d384eca607066e231407279bfcdab10
João Victor Fernandes de Andrade <joaovictorandradis@gmail.com>
2023-09-04 18:53:00 -0300

    implementando JWT!

--- backend/src/routes/routerUser.js
+++ backend/src/routes/routerUser.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/userController');
-
+const token = require('../middleware/jwtToken')
 router.get('/', (req,res) => {res.status(200).send("Bem-vindo à minha API")});
 
 // login e registros:
 router.use('/login', UserController.login);
 router.post('/register', UserController.register);
+
+// Rotas protegidas que requerem token JWT válido:
+router.use(token); // Aplicando o middleware JWT a partir deste ponto
+
 router.get('/user/:nome', UserController.getByName);
 router.get('/user',UserController.getAll);
 router.get('/active', UserController.getAllUsersActives);
