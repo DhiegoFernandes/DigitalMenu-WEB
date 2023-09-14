71bac724e75e289437ca299e27026b6a463ade02
Dhiego <dhiegofs1@gmail.com>
2023-09-14 00:38:20 -0300

    Adicionado tela home, tela mesa, tela login

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,14 +1,61 @@
 import { Link } from "react-router-dom";
-import './Home.css'
+import './Home.css';
+
+import fundo from "./images/fundo.png";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+
 
 function Home() {
+
+  const element = <FontAwesomeIcon icon={faCoffee} />;
+
   return (
     <>
-      <h1 className="titulo">Digital Menu</h1>
-      <nav id='navbar'>
-      <h2><Link to="/loginatendente">Login Atendente</Link></h2>
-      <h2><Link to="/loginmesa">Login Mesa</Link></h2>
-      </nav>
+      <header>
+        <div id="navbar" className="navbar">
+          <h1 className="logo"><span className="color-secondary">D</span>igital <span className="color-secondary">M</span>enu</h1>
+          <nav>
+            <ul>
+              <li><Link className="direciona" to="/loginatendente">Login</Link></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+
+      <main>
+        <section id="tela-login" className="tela-login" >
+          <div className="row flex-items">
+            <div className="column-1">
+              <img className="fundo" src={fundo} />
+            </div>
+            <div className="column-2">
+              <h1>Bem Vindo!</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus numquam quasi maiores voluptatem eius
+                modi sint tempore dolorem ipsam amet consequatur similique pariatur quod, a ullam dolore aperiam ratione
+                nulla?</p>
+              <button className="btn-primary"><Link className="direciona" to="/loginmesa">Iniciar Pedido</Link></button>
+
+
+            </div>
+          </div>
+        </section>
+
+
+      </main>
+
+      <footer className="borda">
+        <div className="social-footer">
+          <a href=""><i className="fa-2x">{element}</i></a>
+          <a href=""><i className="fab fa-instagram fa-2x"></i></a>
+          <a href=""><i className="fab fa-twitter fa-2x"></i></a>
+        </div>
+        <p>Copyright</p>
+      </footer>
+
+
     </>
   )
 }
--- frontend/src/pages/LoginAtendente/LoginAtendente.jsx
+++ frontend/src/pages/LoginAtendente/LoginAtendente.jsx
@@ -2,10 +2,14 @@ import { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { MainContext } from "../../context/context";
 
+import './LoginAtendente.css';
+
+import logoDM from "./images/DigitalMenu1.png";
+
 function LoginAtendente() {
 
-  const {autenticacaoAtendente} = useContext(MainContext);
-  
+  const { autenticacaoAtendente } = useContext(MainContext);
+
   const [nome, setNome] = useState("");
   const [senha, setSenha] = useState("");
 
@@ -14,58 +18,86 @@ function LoginAtendente() {
 
   return (
     <>
-      <div>
-        <h2>Login Atendente</h2>
-        <p><Link to="/">voltar</Link></p>
-      </div>
-      <form>
+
+      <header>
+        <div id="navbar" className="navbar">
+          <h1 className="logo"><span className="color-secondary">D</span>igital <span className="color-secondary">M</span>enu</h1>
+        </div>
+      </header>
+
+      <main>
         <div>
-          <input 
-            autoFocus
-            value={nome}
-            onChange={(e) => setNome(e.target.value)}
-            placeholder="Nome"
-            type="text"
-          />
-          <div>
-            <input 
-              value={senha}
-              onChange={(e) => setSenha(e.target.value)}
-              placeholder="Senha"
-              type={tipoSenha}
-            />
-            {tipoSenha === "password" ? (
-              <button
-                type="button"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setTipoSenha("text")
-                }}
-              >
-                mostrar senha
-              </button>
-            ) : (
-              <button
-                type="button"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setTipoSenha("password")
-                }}
-              >
-                esconder senha
-              </button>
-            )}
+          <div className="fotodm">
+            <img src={logoDM} alt="Logo Digital Menu" />
+          </div>
+
+          <div className="dados">
+            <h2 className="texto1">Entre como administrador</h2>
+
+            {/*  <label for="Login">Login</label>
+            <input type="text" id="login" placeholder="Login">
+
+              <label for="Senha">Senha</label>
+              <input type="password" id="senha" placeholder="Senha">
+
+                <input type="submit" value="Entrar"> */}
+            <form>
+              <div>
+                <input
+                  autoFocus
+                  value={nome}
+                  onChange={(e) => setNome(e.target.value)}
+                  placeholder="Nome"
+                  type="text"
+                />
+                <div>
+                  <input
+                    value={senha}
+                    onChange={(e) => setSenha(e.target.value)}
+                    placeholder="Senha"
+                    type={tipoSenha}
+                  />
+                  {tipoSenha === "password" ? (
+                    <button type="button"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setTipoSenha("text")
+                      }} >mostrar senha</button>
+                  ) : (
+                    <button type="button"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setTipoSenha("password")
+                      }}>esconder senha</button>)}
+                </div>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  onClick={(e) => autenticacaoAtendente(e, nome, senha)}>Entrar</button>
+                <p><Link to="/">voltar</Link></p>
+              </div>
+            </form>
+
+
           </div>
         </div>
+
+      </main>
+
+      <footer className="borda">
         <div>
-          <button
-            type="submit"
-            onClick={(e) => autenticacaoAtendente(e, nome, senha)}
-          >
-            Entrar
-          </button>
+          <a href=""><i className="fab fa-facebook fa-2x"></i></a>
+          <a href=""><i className="fab fa-instagram fa-2x"></i></a>
+          <a href=""><i className="fab fa-twitter fa-2x"></i></a>
         </div>
-      </form>
+        <p>Copyright</p>
+      </footer>
+
+
+
+
+
     </>
   )
 }
