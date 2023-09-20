f597e386bf2e8dd0a2b1cbfc8c9659792bf4daf4
Guigite <guicunhaalves@gmail.com>
2023-09-20 00:43:41 -0300

    finalizando token, inicio de ajuste de rotas privadas

--- frontend/src/pages/Sistema/Sistema.jsx
+++ frontend/src/pages/Sistema/Sistema.jsx
@@ -1,6 +1,17 @@
+import {useContext} from "react";
+import {MainContext} from "../../context/context.jsx"
+
 function Sistema(){
+
+    const {logout} = useContext(MainContext);
+
     return (
+        <>
         <h1>Bem-vindo ao Sistema!</h1>
+        <button
+            onClick={logout}
+        >Logout</button>
+        </>
     )
 }
 
