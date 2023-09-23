7adc45b72b5e2edca613ba76ee3fe710bc30082d
João Victor Fernandes de Andrade <joaovictorandradis@gmail.com>
2023-09-22 23:13:56 -0300

    Ajustando localStorage

--- frontend/src/context/context.jsx
+++ frontend/src/context/context.jsx
@@ -1,43 +1,46 @@
-import {createContext, useState} from "react";
-import { useNavigate } from "react-router-dom";
+import React, { createContext, useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import api from "../api";
 
-//Criado o contexto
 export const MainContext = createContext({});
 
-//Provedor principal do contexto
-function MainProvider({ children }){
-
+function MainProvider({ children }) {
     const navigate = useNavigate();
+    const location = useLocation();
 
-    const [valido, setValido] = useState(false)
-    
-    async function autenticacaoAtendente(e, nome, senha){
+    const [valido, setValido] = useState(false);
+
+    async function autenticacaoAtendente(e, nome, senha) {
         e.preventDefault();
         nome = nome.trim();
         senha = senha.trim();
 
-        try{
-            const { data } = await api.post("/login", {nome, senha});
+        try {
+            const { data } = await api.post("/login", { nome, senha });
             localStorage.setItem("chave", JSON.stringify(data.token));
+
             api.defaults.headers.Authorization = `Bearer ${data.token}`;
             setValido(true);
-            navigate("/sistema");
-            console.log(data.token)
-        }catch (e) {
+
+            // Redireciona explicitamente para a página do sistema após o login
+            navigate("/sistema", { replace: true }); // Use 'replace: true' para substituir a entrada do histórico
+
+            // Agora, atualize a página atual no localStorage
+            localStorage.setItem("currentPage", "/sistema");
+        } catch (e) {
             console.log("Erro na autenticação" + e);
         }
     }
-    
-    async function autenticacaoMesa(e, idMesa){
+
+    async function autenticacaoMesa(e, idMesa) {
         e.preventDefault();
         idMesa = idMesa.trim();
 
-        try{
-            const { data } = await api.post("/mesa/check", {idMesa});
+        try {
+            const { data } = await api.post("/mesa/check", { idMesa });
             navigate("/sistema");
-            console.log(data)
-        }catch (e) {
+            console.log(data);
+        } catch (e) {
             console.log("Erro na autenticação" + e);
         }
     }
@@ -45,46 +48,59 @@ function MainProvider({ children }){
     function validaToken() {
         const token = localStorage.getItem("chave");
         if (token) {
-          api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`;
-          api
-            .post("/verifica-token")
-            .then((response) => {
-              setValido(true);
-              console.log(response)
-            })
-            .catch((error) => {
-              api.defaults.headers.Authorization = undefined;
-              localStorage.removeItem("chave");
-              setValido(false);
-              console.log(error)
-            });
-        }}
+            api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`;
+            api
+                .post("/verifica-token")
+                .then((response) => {
+                    setValido(true);
+                })
+                .catch((error) => {
+                    api.defaults.headers.Authorization = undefined;
+                    localStorage.removeItem("chave");
+                    setValido(false);
+                    console.log(error);
+                });
+        }
+    }
 
-    // function logout(){
-    //     setValido(false);
-    //     localStorage.removeItem('chave')
-    //     api.defaults.headers.Authorization = undefined;
-    //     navigate("/")
-    // }
+    function logout() {
+        setValido(false);
+        localStorage.removeItem("chave");
+        localStorage.removeItem("autenticado"); // Remove o estado de autenticação
+        api.defaults.headers.Authorization = undefined;
+        navigate("/");
+    }
 
-    // useEffect(() => {
-    //     validaToken();
-    //   }, []);
+    useEffect(() => {
+        validaToken();
 
-    return(
+        // Verifica se o usuário está autenticado no localStorage
+        const autenticado = localStorage.getItem("autenticado");
+        if (autenticado === "true") {
+            // Redireciona para a página armazenada no localStorage após o login
+            const currentPage = localStorage.getItem("currentPage");
+            console.log(currentPage);
+            if (currentPage) {
+                navigate(currentPage);
+            } else {
+                navigate("/sistema");
+            }
+        }
+    }, []);
+
+    return (
         <MainContext.Provider
             value={{
                 autenticacaoAtendente,
                 autenticacaoMesa,
                 validaToken,
-                // logout,
-                valido
+                logout,
+                valido,
             }}
         >
             {children}
         </MainContext.Provider>
-    )
+    );
 }
 
 export default MainProvider;
-
