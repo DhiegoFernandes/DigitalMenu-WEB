ab18db1312ae9d8cf0cf03990c92c82e624d4da8
João Victor Fernandes de Andrade <joaovictorandradis@gmail.com>
2023-09-25 17:06:03 -0300

    Crianco validacoes para usuario

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -4,8 +4,15 @@ const connection = require('../connection/connections');
 const jwt = require('jsonwebtoken');
 const secretKey = 'chave'; 
 
+const validarNome = /^[a-zA-ZÀ-ú]+([ '-][a-zA-ZÀ-ú]+)*$/
+const validartipoAcesso = ["administrador","atendente"]
+
 exports.login = async (req, res) => {
     const { nome, senha } = req.body;
+
+    if(!nome|| !senha){
+        return res.status(400).json({message : 'Todos os campos devem ser preenchidos'});
+    }
   
     try {
       const user = await UserModel.autenticar(nome, senha);
@@ -28,18 +35,34 @@ exports.login = async (req, res) => {
 exports.register = async (req, res) => {
     const {nome, senha, tipoAcesso} = req.body;
 
+    if(!nome || !senha || !tipoAcesso) {
+        return res.status(400).json({ message: 'Todos os campos devem ser preenchidos'});
+    }
+
+    if(nome.length > 30 || senha.length > 30){
+        return res.status(400).json({ message: 'Quantidade de caracteres para nome e/ou senha inválidos' });
+    }
+
+    if(!validarNome.test(nome)){
+        return res.status(400).json({ message: 'Nome deve conter apensas letras e espaços'});
+    }
+
+    if(!validartipoAcesso.includes(tipoAcesso)){
+        return res.status(400).json({ message: 'Tipo de acesso inválido'});
+    }
+
     try{
         const existUser = await UserModel.verificaExistencia(nome);
 
         if(existUser.length > 0){
-            res.status(404).json({error : 'Usuario ja existe'});
+            res.status(404).json({message : 'Usuario ja existe'});
         }else{
             await UserModel.registrar(nome,senha, tipoAcesso);
-            res.status(201).json({success : 'Registro bem-sucedido'});
+            res.status(201).json({message : 'Registro bem-sucedido'});
         }
     }catch(err){
         console.log('Erro ao registrar',err);
-        res.status(500).json({error : 'Erro ao registrar user'});
+        res.status(500).json({message : 'Erro ao registrar user'});
     }    
 };
 
@@ -51,7 +74,7 @@ exports.getByName = async(req,res)=>{
         res.json(usuario);
     }catch(err){
         console.error(err);
-        res.status(500).json({error:'Usuario nao esta cadastrado'});
+        res.status(500).json({message:'Usuario nao esta cadastrado'});
     }
 };
 
@@ -62,7 +85,7 @@ exports.getAllUsersActives = async(req,res)=>{
         res.json(usuarios);
     }catch(err){
         console.error(err);
-        res.status(500).json({error:'Erro ao pesquisar usuarios ativos'});
+        res.status(500).json({message:'Erro ao pesquisar usuarios ativos'});
     }
 };
 
@@ -73,7 +96,7 @@ exports.getAll = async(req,res)=>{
         res.json(usuario);
     }catch(err){
         console.log(err);
-        res.status(500).json({error:'Erro ao listar todos os usuarios'});
+        res.status(500).json({message:'Erro ao listar todos os usuarios'});
     }    
 };
 
@@ -85,26 +108,39 @@ exports.getCategoriaPorNome = async(req,res)=>{
     }
     catch(err){
         console.log(err);
-        res.status(500).json({error:'Erro ao buscar usuario'});
+        res.status(500).json({message:'Erro ao buscar usuario'});
     }
 };
 
 exports.put = async (req, res) => {
     const { nome } = req.params;
     const { senha } = req.body; 
+
+    if(!nome || !senha){
+        return res.status(404).json({message: 'Todos os campos devem ser enviados'});
+    }
+    
+    if(nome.length > 30 || senha.length > 30){
+        return res.status(400).json({ message: 'Quantidade de caracteres para nome e/ou senha inválidos' });
+    }
+
+    if(!validarNome.test(nome)){
+        return res.status(400).json({ message: 'Nome deve conter apensas letras e espaços'});
+    }
+
     
     try {
         const usuario = await UserModel.buscarPorUsuario(nome);
         if (!usuario) {
-            res.status(404).json({ error: "Usuário não encontrado" });
+            res.status(404).json({ message: "Usuário não encontrado" });
             return;
         }
 
         await UserModel.atualizarSenha(nome, senha);
-        res.status(200).json({ success: "Senha atualizada com sucesso!" });
+        res.status(200).json({ message: "Senha atualizada com sucesso!" });
     } catch (err) {
         console.error(err);
-        res.status(500).json({ error: "Erro ao atualizar senha do usuário" });
+        res.status(500).json({ message: "Erro ao atualizar senha do usuário" });
     }
 };
 
@@ -114,14 +150,14 @@ exports.delete = async(req,res)=>{
     
     try{
         if(!existUser){
-            res.status(500).json({error : 'Usuario nao existe'});     
+            res.status(500).json({message : 'Usuario nao existe'});     
         }else{
             await UserModel.delete(nome);
-            res.status(200).json({success : 'Usuário deletado com sucesso!'});
+            res.status(200).json({message : 'Usuário deletado com sucesso!'});
         }
     }catch(err){
         console.error(err);
-        res.status(500).json({error:'Erro ao deletar usuário'});
+        res.status(500).json({message:'Erro ao deletar usuário'});
     }
 };
 
